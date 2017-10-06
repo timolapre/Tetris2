444188df8c32038e1ff17f84a4b6d57238642312
timolapre <32130004+timolapre@users.noreply.github.com>
2017-10-06 16:25:23 +0200

    Add files via upload

---
+++ Tetris2/Tetris2/Block.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris2.Content
+{
+    class Block
+    {
+        Texture2D texture;
+        int blockID;
+        int[,] table;
+        int posX = 0;
+        int posY = 0;
+        int rotation = 0;
+        int timer;
+        bool falling = true;
+        Game1 game;
+
+        public Block(Texture2D newtexture, int block, Game1 game)
+        {
+            texture = newtexture;
+            blockID = block;
+            BlockDraw();
+            this.game = game;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            if (falling == true)
+                for (int i = 0; i < 3; i++)
+                    for (int x = 0; x < 3; x++)
+                        if (table[x, i] == 1)
+                            spritebatch.Draw(texture, new Vector2((posX + i) * texture.Width, (posY + x) * texture.Height), Color.White);
+        }
+
+        public void BlockUpdate()
+        {
+            timer++;
+            //Falling
+            if (timer > 50 && falling)
+            {
+                posY++;
+                timer = 0;
+            }
+
+            //Rotation
+            if (Game1.currentkeyboardstate.IsKeyDown(Keys.Down) && Game1.previouskeyboardstate.IsKeyUp(Keys.Down))
+                rotation = (rotation + 1) % 4;
+
+            //Moving
+            if (Game1.currentkeyboardstate.IsKeyDown(Keys.Left) && Game1.previouskeyboardstate.IsKeyUp(Keys.Left))
+                posX = MathHelper.Max(posX - 1, 0);
+            if (Game1.currentkeyboardstate.IsKeyDown(Keys.Right) && Game1.previouskeyboardstate.IsKeyUp(Keys.Right))
+                posX = MathHelper.Min(posX + 1, Game1.tablewidth - 3);
+
+
+            if (falling)
+            {
+                if ((Game1.TetrisTable[posX, posY + 3] == 1 && table[0, 2] == 1) || (Game1.TetrisTable[posX + 1, posY + 3] == 1 && table[1, 2] == 1) || (Game1.TetrisTable[posX + 2, posY + 3] == 1 && table[2, 2] == 1))
+                {
+                    falling = false;
+                    for (int i = 0; i < 3; i++)
+                        for (int x = 0; x < 3; x++)
+                            if (table[i, x] == 1)
+                                Game1.TetrisTable[posX + x, posY + i] = 1;
+                    Game1.createnewblock = 1;
+                }
+                if ((Game1.TetrisTable[posX, posY + 2] == 1 && table[0, 1] == 1) || (Game1.TetrisTable[posX + 1, posY + 2] == 1 && table[1, 1] == 1) || (Game1.TetrisTable[posX + 2, posY + 2] == 1 && table[2, 1] == 1))
+                {
+                    falling = false;
+                    for (int i = 0; i < 3; i++)
+                        for (int x = 0; x < 3; x++)
+                            if (table[i, x] == 1)
+                                Game1.TetrisTable[posX + x, posY + i] = 1;
+                    Game1.createnewblock = 1;
+                }
+            }
+        }
+
+        public void BlockDraw()
+        {
+            //BlockID = 1
+            if (blockID == 1)
+            {
+                if (rotation == 0)
+                {
+                    table = new int[,] {
+                                        {0,0,0,} ,
+                                        {1,1,1,} ,
+                                        {0,1,0,} ,
+                                    };
+                }
+                else if (rotation == 1)
+                {
+                    table = new int[,] {
+                                        {0,1,0,} ,
+                                        {0,1,1,} ,
+                                        {0,1,0,} ,
+                                    };
+                }
+                else if (rotation == 2)
+                {
+                    table = new int[,] {
+                                        {0,1,0,} ,
+                                        {1,1,1,} ,
+                                        {0,0,0,} ,
+                                    };
+                }
+                else if (rotation == 3)
+                {
+                    table = new int[,] {
+                                        {0,1,0,} ,
+                                        {1,1,0,} ,
+                                        {0,1,0,} ,
+                                    };
+                }
+            }
+
+
+            //BlockID = 2
+            if (blockID == 2)
+            {
+                if (rotation == 0)
+                {
+                    table = new int[,] {
+                                        {0,1,0,} ,
+                                        {0,1,0,} ,
+                                        {0,1,1,} ,
+                                    };
+                }
+                else if (rotation == 1)
+                {
+                    table = new int[,] {
+                                        {0,0,1,} ,
+                                        {1,1,1,} ,
+                                        {0,0,0,} ,
+                                    };
+                }
+                else if (rotation == 2)
+                {
+                    table = new int[,] {
+                                        {1,1,0,} ,
+                                        {0,1,0,} ,
+                                        {0,1,0,} ,
+                                    };
+                }
+                else if (rotation == 3)
+                {
+                    table = new int[,] {
+                                        {0,0,0,} ,
+                                        {1,1,1,} ,
+                                        {1,0,0,} ,
+                                    };
+                }
+            }
+        }
+    }
+}
---
+++ Tetris2/Tetris2/Game1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Tetris2
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        static public KeyboardState currentkeyboardstate;
+        static public KeyboardState previouskeyboardstate;
+        static public int tablewidth = 11;
+        static public int tableheight = 15;
+        static public int createnewblock;
+
+        Texture2D blocktexture;
+
+        static public int[,] TetrisTable;
+
+        //Content.Block block;
+        List<Content.Block> blocklist = new List<Content.Block>();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 600;
+            graphics.ApplyChanges();
+
+            TetrisTable = new int[tablewidth, tableheight];
+
+            //block = new Content.Block(Content.Load<Texture2D>("block"), 1, this);
+
+            blocktexture = Content.Load<Texture2D>("block");
+            blocklist.Add(new Content.Block(Content.Load<Texture2D>("block"), 2, this));
+
+            for (int i = 0; i < tablewidth; i++)
+                TetrisTable[i, tableheight-1] = 1;
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.P)) Debugger.Break();
+            //if (Keyboard.GetState().IsKeyDown(Keys.F)) graphics.IsFullScreen = true;
+
+            previouskeyboardstate = currentkeyboardstate;
+            currentkeyboardstate = Keyboard.GetState();
+
+            if(createnewblock == 0)
+            {
+                foreach (Content.Block block in blocklist)
+                {
+                    block.BlockUpdate();
+                    block.BlockDraw();
+                }
+            }
+            else
+            {
+                NewBlock();
+                createnewblock = 0;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            foreach(Content.Block block in blocklist)
+                block.Draw(spriteBatch);
+            for (int i = 0; i < tableheight; i++)
+                for (int x = 0; x < tablewidth; x++)
+                    if (TetrisTable[x, i] == 1)
+                        spriteBatch.Draw(blocktexture, new Vector2(x * blocktexture.Width, i * blocktexture.Height), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void NewBlock()
+        {
+            blocklist.Add(new Content.Block(Content.Load<Texture2D>("block"), 1, this));
+        }
+    }
+}
